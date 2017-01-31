29a87ffdd5b7be464cf9e87bf214b98e02a66db6
Cassie Chapman <cassiechapman@148-100-137-190.FoxNet.marist.edu>
2017-01-31 11:29:21 -0500

    comment details added and testing completed

--- labs/0/Grades0.java
+++ labs/0/Grades0.java
@@ -1,9 +1,19 @@
+/*
+file: Grades0.java
+author: Cassie Schaaf, SD1, January 31
+course: CMPT 200
+assignment: lab 0
+due date: January 31
+version: 1.4
+
+This file contains the code to calculate the final course grade based on the CMPT220 rubric
+*/
 import java.util.Scanner;
 
 public class Grades0 {
   public static void main(String[] args) {
-  //Cassie Schaaf, SD1, January 31
 
+    //declare variables
     double homework;
     double midterm;
     double project;
@@ -11,6 +21,7 @@ public class Grades0 {
     double total;
     double homeworkPercent = 0.4;
 
+    //Request input for rubric sections
     Scanner input = new Scanner(System.in);
     System.out.println("Enter the grade for Homework & Labs: ");
     homework = input.nextDouble();
@@ -24,8 +35,11 @@ public class Grades0 {
     System.out.println("Enter the grade for the Final exam: ");
     finals = input.nextDouble();
 
+    //Rubric defines the following percentages: Hwk 40%, Prj/MT/F all 20%. Values below
+    //Calculation of final course grade
     total = (homework * 0.4) + (project * 0.2) + (midterm * 0.2) + (finals * 0.2);
 
+    //display final course grade
     System.out.println("Final Grade for Software Development I: " + total);
   }
 }
--- labs/0/Pattern0.java
+++ labs/0/Pattern0.java
@@ -1,3 +1,13 @@
+/*
+file: Pattern0.java
+author: Cassie Schaaf, SD1, January 31
+course: CMPT 200
+assignment: lab
+due date: January 31
+version: 1.4
+
+This file contains the code to print the word "JAVA" in the required format
+*/
 public class Pattern0 {
   public static void main(String[] args){
     //Display Hello World! message
