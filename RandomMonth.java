8cd117e075e000723b355a1e1b2876dd18e5ff54
BeingGeeky <craychap@gmail.com>
2017-02-06 17:42:35 -0500

    update

---
+++ RandomMonth.java
@@ -0,0 +1,24 @@
+/*
+file: 3.4 randomMonth.java
+author: Cassie Schaaf, SD1, February 3
+course: CMPT 220
+assignment: lab 2-3.4
+due date: February 7, 2017
+version: 1.0
+
+This file contains the code to Lab 2: Problem 3.4 Random Month generator and text display using if and/or if-else statements
+*/
+import java.util.Scanner;
+
+public class RandomMonth {
+  public static void main(String[] args){
+
+//declare variables
+  double month;
+
+//random number generator
+  month = Math.random()*12;
+
+  System.out.println(month);
+  }
+}
