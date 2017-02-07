e1a01c42676d667f07b4026229d378d3e5838a82
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-02-06 22:20:32 -0500

    polygon done

--- labs/2/areaPolygon.java
+++ labs/2/areaPolygon.java
@@ -11,9 +11,26 @@ This file contains the code to lab 2 - problem 4.5 area of a regular polygon. Us
 
 import java.util.Scanner;
 
-public class areaPolygon{
+public class AreaPolygon{
   public static void main(String[] args){
 
     //declare variables
+    final double PI = 3.14159;
+
+    //Call for input of number of sides
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the number of sides.");
+
+    //get input for sides
+    double n = input.nextDouble();
+
+    //get input for length of sides
+    System.out.println("Enter the length of a side.");
+    double s = input.nextDouble();
+
+    //area of the polygon
+    double area = (n * Math.pow(s,2))/(4 * Math.tan(PI/n));
+
+    System.out.println("The area of the polygon is " + area);
   }
 }
