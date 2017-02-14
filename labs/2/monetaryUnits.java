7fe536267f30c683fc7630ef4ca487393994ef06
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-02-13 20:16:12 -0500

    monetary units works

--- labs/2/monetaryUnits.java
+++ labs/2/monetaryUnits.java
@@ -19,29 +19,32 @@ public class MonetaryUnits{
 
     // Receive the amount
     System.out.print(
-    "Enter an amount in double, for example 11.56: ");
-    double amount = input.nextDouble();
+    "Enter a dollar amount, including cents. For example, 11.56: ");
+    //double amount = input.nextDouble();
+    String amount = input.next();
+    double dollarAmount= Double.parseDouble(amount);
 
-    int remainingAmount = (int)(amount * 100);
+    //pull out thousands, hundreds, tens, ones, & decimal places
+    double remainingAmount = dollarAmount * 100;
 
     // Find the number of one dollars
-    int numberOfOneDollars = remainingAmount / 100;
+    double numberOfOneDollars = Math.floor(remainingAmount / 100);
     remainingAmount = remainingAmount % 100;
 
     // Find the number of quarters in the remaining amount
-    int numberOfQuarters = remainingAmount / 25;
+    double numberOfQuarters = Math.floor(remainingAmount / 25);
     remainingAmount = remainingAmount % 25;
 
     // Find the number of dimes in the remaining amount
-    int numberOfDimes = remainingAmount / 10;
+    double numberOfDimes = Math.floor(remainingAmount / 10);
     remainingAmount = remainingAmount % 10;
 
     // Find the number of nickels in the remaining amount
-    int numberOfNickels = remainingAmount / 5;
+    double numberOfNickels = Math.floor(remainingAmount / 5);
     remainingAmount = remainingAmount % 5;
 
     // Find the number of pennies in the remaining amount
-    int numberOfPennies = remainingAmount;
+    double numberOfPennies = remainingAmount;
 
     // Display results
     System.out.println("Your amount " + amount + " consists of");
@@ -51,5 +54,6 @@ public class MonetaryUnits{
     System.out.println("    " + numberOfNickels + " nickels");
     System.out.println("    " + numberOfPennies + " pennies");
 
+    //System.out.println(numberOfOneDollars);
   }
 }
