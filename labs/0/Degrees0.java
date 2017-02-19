064a169084d4fff3b2d0c098a23815a5dba596a9
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-02-19 12:12:26 -0500

    format fix

--- labs/0/Degrees0.java
+++ labs/0/Degrees0.java
@@ -12,19 +12,19 @@ import java.util.Scanner;
 
 public class Degrees0 {
   public static void main(String[] args){
-  //Cassie Schaaf, SD1, January 31
+    //Cassie Schaaf, SD1, January 31
 
-  //declare variables
-  double celsius;
-  double fahrenheit;
-  
-  //Print text to request input of Celius value
-  System.out.println("Enter a degree in Celsius: ");
-  Scanner input = new Scanner(System.in);
-  celsius = input.nextDouble();
+    //declare variables
+    double celsius;
+    double fahrenheit;
 
-  //fahrenheit calculation
-  fahrenheit = (1.8 * celsius) + 32;
-  System.out.println(celsius + " Celsius is " + fahrenheit + " Fahrenheit");
+    //Print text to request input of Celius value
+    System.out.println("Enter a degree in Celsius: ");
+    Scanner input = new Scanner(System.in);
+    celsius = input.nextDouble();
+
+    //fahrenheit calculation
+    fahrenheit = (1.8 * celsius) + 32;
+    System.out.println(celsius + " Celsius is " + fahrenheit + " Fahrenheit");
   }
 }
