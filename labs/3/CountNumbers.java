80eaf586533f0372c18d9f2b1d1fd901faa39ca2
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-02-20 21:44:26 -0500

    future tuition

---
+++ labs/3/CountNumbers.java
@@ -0,0 +1,22 @@
+/*
+file: CountNumbers.java
+author: Cassie Schaaf
+course: CMPT 220
+assignment: lab 3-5.1
+due date: February 21, 2017
+version: 1.0
+
+This file contains the code to lab 3- problem 5.1. Count positive & negative numbers and give average
+*/
+
+import java.util.Scanner;
+
+public class CountNumbers {
+  public static void main(String[] args){
+
+
+    //Print answer
+    System.out.println("The area of the pentagon is " + area);
+
+  }
+}
---
+++ labs/3/LargeN.java
@@ -0,0 +1,22 @@
+/*
+file: LargeN.java
+author: Cassie Schaaf
+course: CMPT 220
+assignment: lab 3-5.13
+due date: February 21, 2017
+version: 1.0
+
+This file contains the code to lab 3- problem 5.13. Find largest n such that n2 is less than 12,000. Use while loop.
+*/
+
+import java.util.Scanner;
+
+public class LargeN {
+  public static void main(String[] args){
+
+
+    //Print answer
+    System.out.println("The area of the pentagon is " + area);
+
+  }
+}
