2f1faaf2acbc112790b5fb47cce259bcd0eeedfb
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-02-26 19:06:38 -0500

    count numbers

--- labs/3/CountNumbers.java
+++ labs/3/CountNumbers.java
@@ -14,9 +14,32 @@ import java.util.Scanner;
 public class CountNumbers {
   public static void main(String[] args){
 
+    //variables & inputs
+    Scanner input = new Scanner(System.in);
+    int pos =0;
+    int neg =0;
+    int count = 0;
+    double total=0.0;
+    int num;
+
+    //do while to count numbers
+    do {
+      num = input.nextInt();
+      if (num > 0){
+        pos++;
+      } else if (num <0) {
+        neg++;
+      }
+      count++;
+      total=num+total;
+    } while(num != 0);
+    //calc average
+    double average = total/(count-1);
 
     //Print answer
-    System.out.println("The area of the pentagon is " + area);
-
+    System.out.println("The number of positives is " + pos);
+    System.out.println("The number of negatives is "+ neg);
+    System.out.println("The total is " + total);
+    System.out.println("The average is " + average);
   }
 }
