12f73e07e2b660f1ef0dc1515e06be3b22084597
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-04-02 19:42:08 -0400

    first push project 2

---
+++ projects/2/Prj2_schaaf.java
@@ -0,0 +1,63 @@
+/*
+file: Prj2_schaaf.java
+author: Cassie Schaaf
+course: CMPT 220
+assignment: Project 2
+due date: May 2, 2017
+version: 1.0
+
+This file contains the code to clean heart rate data
+*/
+
+import java.util.Scanner;
+
+public class Prj2_schaaf {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    /* This is the code for the Art Inspiration app. The app:
+    - displays a drop-down list of art subject, techniques, and mediums
+    - User selects n options from the lists
+    - Once selected, the user click "Inspire Me" button
+    - Button launches a random generator
+    - The app then displays n terms or phrases within the selected lists
+
+   For example,
+     * user drops down the list and selects MEDIUM & ANIMAL
+     * User clicks INSPIRE ME
+     * App displays "Watercolor Rabbit"
+     * If user clicks INSPIRE ME again, app displays "Fingerpaint Dolphin"
+
+   Stage 2:
+     - user create an "account" of name, username, password, & details
+     - save favorite ideas
+ */
+    
+    if (select==0){
+    	
+    } else {
+    	
+    }
+
+    System.out.println("Try this:" + result);
+    }
+
+  public static String[] convolveVecs(int[] first, int [] second) {
+   
+	
+	//Selections 
+	ideas = new String[][] [ // ,
+	      		/*lists*/ 
+	      		/*0 Mediums*/ ["Pastel","Oil","Watercolor","Acrylic","Colored Pencils","Charcoal"],
+	            /*1 Jungle*/ ["Canopy","Monkeys","Snakes","Jaguar",""],
+	      		/*2 Lanscape*/ ["Ocean","Sunset","Forest","Mountains",""],
+	      		/*3 Forest*/ ["Tree","Stream","Leaves","","",""],
+	      		/*4 Country*/ ["Ireland","Egypt","China","India","Russia"],
+	      		/*5 Insect*/ ["Spider","Centipede","","",""],
+	]
+    
+    int randomNum = int Math.random();
+	System.out.print(randomNum);
+    return result;  
+
+   }
+}
--- projects/2/proj2/app/src/main/java/com/example/cassiechapman/proj2/MainActivity.java
+++ projects/2/proj2/app/src/main/java/com/example/cassiechapman/proj2/MainActivity.java
@@ -9,5 +9,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* This is the code for the Art Inspiration app. The app:
+            - displays a drop-down list of art subject, techniques, and mediums
+            - User selects n options from the lists
+            - Once selected, the user click "Inspire Me" button
+            - Button launches a random generator
+            - The app then displays n terms or phrases within the selected lists
+
+           For example,
+             * user drops down the list and selects MEDIUM & ANIMAL
+             * User clicks INSPIRE ME
+             * App displays "Watercolor Rabbit"
+             * If user clicks INSPIRE ME again, app displays "Fingerpaint Dolphin"
+
+           Stage 2:
+             - user create an "account" of name, username, password, & details
+             - save favorite ideas
+         */
+
+        System.out.println("Try this:");
+        
+
+
     }
 }
