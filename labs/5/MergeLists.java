fe114f3fee069052646f698211a8e0926bd9a1a6
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-05-07 14:43:03 -0400

    MergeLists DONE

--- labs/5/MergeLists.java
+++ labs/5/MergeLists.java
@@ -25,63 +25,64 @@ import java.util.Scanner;
 public class MergeLists {
   public static void main(String[] args){
 
-System.out.println("Enter an array length and array elements for the first array.");
-	Scanner input = new Scanner(System.in);
-int firstLength = input.nextInt();
-int[] firstList = new int[firstLength];
+    System.out.println("Enter an array length and array elements for the first array.");
+	    Scanner input = new Scanner(System.in);
+    int firstLength = input.nextInt();
+    int[] firstList = new int[firstLength];
 
-for  (int p = 0; p<firstList.length; p++){
-  firstList[p] = input.nextInt();
-}
-System.out.println("Enter an array length and array elements for the second array.");
-int secondLength = input.nextInt();
-int[] secondList = new int[secondLength];
+    for (int p = 0; p<firstList.length; p++){
+      firstList[p] = input.nextInt();
+    }//end for loop
+    
+    System.out.println("Enter an array length and array elements for the second array.");
+    int secondLength = input.nextInt();
+    int[] secondList = new int[secondLength];
 
-for  (int p = 0; p<secondList.length; p++){
-  secondList[p] = input.nextInt();
-}
+    for  (int p = 0; p<secondList.length; p++){
+      secondList[p] = input.nextInt();
+    }//end for loop
 
-//sort array
-int[] combinedArray = merge(firstList, secondList);
+    //sort array
+    int[] combinedArray = merge(firstList, secondList);
 
-//sort array
-bubbleSort(combinedArray);
+    //sort array
+    bubbleSort(combinedArray);
 
-//print array
-printArray(combinedArray, combinedArray.length);
+    //print array
+    printArray(combinedArray, combinedArray.length);
 
   }//end main method
 
-public static void printArray(int[] array, int arrayLength){
-  //method to print array
-  System.out.println("\nThe array is ");
-  for (int n=0; n < arrayLength; n++){
-    System.out.print(array[n] + " ");
+  public static void printArray(int[] array, int arrayLength){
+    //method to print array
+    System.out.println("\nThe array is ");
+    for (int n=0; n < arrayLength; n++){
+      System.out.print(array[n] + " ");
     }//end for statement
-}//end print array method
+  }//end print array method
 
-public static void bubbleSort(int[] array){
-  //method to sort array
-  int n = array.length;
-  int temp = 0;
-  for (int i = 0; i < n; i++){
-    for (int j=1; j < (n-i); j++){
-      if (array[j-1] > array[j]){
-        temp = array[j-1];
-        array[j-1] = array[j];
-        array[j] = temp;
-      }//end if statement 
-    }//end of inner for loop
-  }//end for loop
-}//end bubbleSort method
+  public static void bubbleSort(int[] array){
+    //method to sort array
+    int n = array.length;
+    int temp = 0;
+    for (int i = 0; i < n; i++){
+      for (int j=1; j < (n-i); j++){
+        if (array[j-1] > array[j]){
+          temp = array[j-1];
+          array[j-1] = array[j];
+          array[j] = temp;
+        }//end if statement 
+      }//end of inner for loop
+    }//end for loop
+  }//end bubbleSort method
 
-public static int[] merge(int[] list1, int[] list2){
-  int[] comboList = new int[list1.length + list2.length];
-  System.arraycopy(list1, 0, comboList, 0, list1.length);
-  System.arraycopy(list2, 0, comboList, list1.length, list2.length);
-  int comboLength = comboList.length;
+  public static int[] merge(int[] list1, int[] list2){
+    int[] comboList = new int[list1.length + list2.length];
+    System.arraycopy(list1, 0, comboList, 0, list1.length);
+    System.arraycopy(list2, 0, comboList, list1.length, list2.length);
+    int comboLength = comboList.length;
   
-  return comboList;
-}//end merge method
+    return comboList;
+  }//end merge method
 }//end program
   
