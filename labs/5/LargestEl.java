13afb8e2717db8e76e680171cf569a482c2108a7
Cassie Chapman <cassiechapman@Cassies-MacBook-Air.local>
2017-05-09 01:41:26 -0400

    lab 5 done

--- labs/5/LargestEl.java
+++ labs/5/LargestEl.java
@@ -8,61 +8,56 @@ version: 1.0
 
 This file contains the code to 8.13 Largest element in array
 /*********************************************************************************
-* (Locate the largest element) Write the following method that returns the       *
-* location of the largest element in a two-dimensional array.                    *
-*                                                                                *
-* public static int[] locateLargest(double[][] a)                                *
-*                                                                                *
-* The return value is a one-dimensional array that contains two elements. These  *
+* (Locate the largest element) Write the following method that returns the *
+* location of the largest element in a two-dimensional array.*
+**
+* public static int[] locateLargest(double[][] a)*
+**
+* The return value is a one-dimensional array that contains two elements. These*
 * two elements indicate the row and column indices of the largest element in the *
-* two-dimensional array. Write a test program that prompts the user to enter a   *
-* two-dimensional array and displays the location of the largest element in the  *
-* array.                                                                         *
+* two-dimensional array. Write a test program that prompts the user to enter a *
+* two-dimensional array and displays the location of the largest element in the*
+* array. *
 *********************************************************************************/
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class LargestEl {
   public static void main(String[] vargs){
-    
-	Scanner input = new Scanner(System.in);
-int row = 0;
-int column = 0;
 
-int[][] matrix = new int[row][column];
+    	Scanner input = new Scanner(System.in);
 
-	System.out.println("Enter the row and column numbers.");
-row = input.nextInt();
-column = input.nextInt();
+	    System.out.println("Enter the array parameters:");
+    int rowL = input.nextInt();
+    int columnL = input.nextInt();
 
-	System.out.println("Enter " + matrix.length + " rows " + matrix[0].length );	
- 
-	  for  (int n = 0; n< row-1; n++){
-	    int p=0;
-    matrix[n][p] = input.nextInt();
-    for (p = 0; p<column-1;p++){
-      matrix[n][p] = input.nextInt();
-    }
-  	}
-	  
-	  int maxRow=0;
-	  int indexOfMaxRow = 0;
-	  
-	  for(column=0; column<matrix[0].length; column++){
-	    maxRow += matrix[0][column];
-	  }
-	  
-	  for (row = 1; row < matrix.length; row++){
-	    int totalOfThisRow = 0;
-	    for (column = 0; column < matrix[row].length; column++){
-	      totalOfThisRow += matrix[row][column];
-	    }
-	    if (totalOfThisRow > maxRow){
-	      maxRow = totalOfThisRow;
-	      indexOfMaxRow = row;
-	    }
-	  }
-	  System.out.println("Maximum value is: ");  
+    double [][] matrix = new double[rowL][columnL];
+
+	    System.out.println("Enter" + rowL + " rows & " + columnL + " columns");	
+
+    //fill array
+    for (int i=0; i < matrix.length; i++){
+      for (int n = 0; n < matrix[i].length; n++){
+        matrix[i][n] = input.nextDouble();
+      }//inner for loop
+    }//outer for loop
+
+	    int maxRow=0;
+	    int indexOfMaxRow = 0;
+	    int r=0;
+	    int c=1;
+	
+	    for(int x = 0; x < matrix.length; x++){
+	      for (int y = 0; y < matrix[x].length; y++){
+	        if (matrix[maxRow][indexOfMaxRow] < matrix[x][y]){
+	          maxRow=x;
+	          indexOfMaxRow=y;
+	        }//if loop
+      	}//end inner for loop
+    }//end for loop
+	
+	    System.out.println("The location of the largest element is: ("+maxRow +", "+ indexOfMaxRow+")");
   }//end main class
 }//end program
-  
+
